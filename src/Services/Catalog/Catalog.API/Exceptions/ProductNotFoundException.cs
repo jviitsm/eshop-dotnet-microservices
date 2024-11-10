6095fb6654c9732c62381b7b9a17c862d2e5d200
jvictorm13 <jvictorgmodesto@gmail.com>
2024-11-09 22:51:53 -0300

    get product by id implemented

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException(Guid id)
+            : base($"Product with ID '{id}' not found!")
+        {
+        }
+    }
+}
