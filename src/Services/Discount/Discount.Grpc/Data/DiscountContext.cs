00786fb5927062881c0613b76953ff4f612538e9
jvictorm13 <jvictorgmodesto@gmail.com>
2024-12-15 23:37:26 -0300

    iniciando service

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -15,10 +15,8 @@ namespace Discount.Grpc.Data
         {
             modelBuilder.Entity<Coupon>().HasData(
                 new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
-                new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 },
+                new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
                 );
         }
-
-
     }
 }
--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,17 +1,37 @@
-﻿using Grpc.Core;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services
 {
-    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    public class DiscountService
+        (DiscountContext discountContext, ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
     {
-        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
         {
-            return base.GetDiscount(request, context);
+            var coupon = await discountContext.Coupons
+                .FirstOrDefaultAsync(c => c.ProductName == request.ProductName)
+                ?? new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount" };
+
+            logger.LogInformation("Discount retrieved: ProductName={ProductName}, Amount={Amount}", coupon.ProductName, coupon.Amount);
+
+            return coupon.Adapt<CouponModel>();
         }
 
-        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
         {
-            return base.CreateDiscount(request, context);
+            var coupon = request.Coupon?.Adapt<Coupon>()
+                ?? throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid Discount Request"));
+
+            await discountContext.Coupons.AddAsync(coupon);
+            await discountContext.SaveChangesAsync();
+
+            logger.LogInformation("Discount created: ProductName={ProductName}", coupon.ProductName);
+
+            return coupon.Adapt<CouponModel>();
         }
 
         public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
